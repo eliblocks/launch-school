b907f745e1f54e6de9be0c79a86e2662d6fe140e
Eli <eliblock@yahoo.com>
2016-11-15 18:00:19 +0000

    variables

---
+++ age.rb
@@ -0,0 +1,10 @@
+def future_age
+  puts "How old are you"
+  age = gets.to_i
+  puts "You will be #{age + 10} in 10 years"
+  puts "You will be #{age + 20} in 20 years"
+  puts "You will be #{age + 30} in 30 years"
+  puts "You will be #{age + 40} in 40 years"
+end
+
+age
---
+++ name.rb
@@ -0,0 +1,21 @@
+#1 
+puts "What is your first name?"
+fname = gets.chomp
+puts "What is your last name?"
+lname = gets.chomp
+
+def greeting 
+  puts "Hello, #{fname} #{lname}!"
+end
+
+def repeat
+  10.times do { puts name }
+end
+
+
+
+
+
+
+
+
