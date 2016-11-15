554a62a3cd9ad2ca91c02b4341339ee02947d4db
Eli <eliblock@yahoo.com>
2016-11-15 17:30:48 +0000

    basics

---
+++ basics.rb
@@ -0,0 +1,45 @@
+#1
+
+puts "Eli " + "Block"
+
+#2
+
+1050 / 1000 
+1050 % 1000 / 100
+1050 % 100 / 10
+1050 % 1000 % 100 % 10
+
+#3
+
+movies = { godfather: 1972,
+            shawshank: 1994,
+            titanic: 1997 }
+
+puts movies[:godfather]
+puts movies[:shawshank]
+puts movies[:titanic]
+
+#4
+
+movies = [1972, 1994, 1997]
+
+puts movies[0]
+puts movies[1]
+puts movies[2]
+
+#5
+
+puts fac5 =  5 * 4 * 3 * 2 * 1
+puts fac6 = 6 * fac5
+puts fac7 = 7 * fac6
+puts = 8 * fac7
+
+#6
+
+puts 5.2 ** 2
+puts 6.3 ** 2
+puts 3.9 ** 2
+
+#7
+
+#forgot closing bracket
